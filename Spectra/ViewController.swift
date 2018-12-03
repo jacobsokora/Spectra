c805109dcf2debe8c52fb0bd5bc8cb22b64eae0a
Jacob <jacobsokora@gmail.com>
2018-12-03 16:51:58 -0600

    Add base project

---
+++ Spectra/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Spectra
+//
+//  Created by Jacob Sokora on 12/3/18.
+//  Copyright © 2018 Jacob Sokora. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
